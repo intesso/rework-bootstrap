ad99ac02f4763b96a638d232a47448bed6cd05cd
Andi Neck <andi.neck@intesso.com>
2013-09-20 13:54:37 +0200

    fixed html and body replacements

--- index.js
+++ index.js
@@ -5,9 +5,13 @@ module.exports = function bootstrap(str) {
       if (!rule.selectors) return rule;
 
       rule.selectors = rule.selectors.map(function(selector) {
-        if (selector.indexOf('html') === 0 || selector.indexOf('body') === 0) {
-          // append
-          return selector + ' ' + str;
+        if (selector.indexOf('html') === 0) {
+          // replace
+          return selector.replace('html', 'html ' + str);
+        }
+        if (selector.indexOf('body') === 0) {
+          // replace
+          return selector.replace('body', 'body ' + str);
         } else if (selector.indexOf('.modal-backdrop') >= 0 || selector.indexOf('.fade') >= 0) {
           // ignore
           return selector;
